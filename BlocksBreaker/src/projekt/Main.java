b344fa6b5c9e03cc45ca547518deddd5f3a3cb60
IzabelaFejfer <izabela.fejfer@wp.pl>
2022-03-28 16:11:32 +0200

    Dodano Interfejs

---
+++ BlocksBreaker/src/projekt/Main.java
@@ -0,0 +1,83 @@
+package projekt;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+
+
+
+public class Main extends JFrame {
+	JPanel panel1, panel2;
+	JButton button1, button2, button3 ;
+	JMenuBar menubar;
+	JMenu menu;
+	JMenuItem item1, item2;
+	JRadioButton radiobutton1, radiobutton2;
+	ButtonGroup grupa;
+	JLabel scorelabel, sclabel;
+	int score=0;
+	int fontSize=20;
+	Font font=new Font("Helvetica", Font.BOLD, fontSize);
+	Main(){
+	this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    this.setSize(900,600);
+    this.setTitle("BlocksBreaker");
+    this.setLayout(new BorderLayout());
+    this.setResizable(false);
+    
+    //œrodkowy panel
+    panel2= new JPanel();
+    panel2.setBackground(Color.white);
+    this.add(panel2, BorderLayout.CENTER);
+    
+    //prawy panel
+    panel1=new JPanel();
+    panel1.setLayout(new GridLayout(8,1, 15, 15));
+    Color color1 = new Color(176, 224, 230);
+    Color color2 = new Color(255, 200, 255);
+    panel1.setBackground(color1);
+    scorelabel=new JLabel("Punkty:");
+    scorelabel.setFont(font);
+    scorelabel.setVerticalAlignment(SwingConstants.CENTER);
+    scorelabel.setHorizontalAlignment(SwingConstants.CENTER);
+    sclabel=new JLabel(""+score);
+    sclabel.setFont(font);
+    sclabel.setVerticalAlignment(SwingConstants.CENTER);
+    sclabel.setHorizontalAlignment(SwingConstants.CENTER);
+    radiobutton1 = new JRadioButton("Tryb jasny");
+    radiobutton1.setBackground(color1);
+    radiobutton2 = new JRadioButton("Tryb ciemny");
+    radiobutton2.setBackground(color1);
+    button1 = new JButton("Nowa Gra");
+    button1.setBackground(color2);
+    button2 = new JButton("Zatrzymaj grê");
+    button2.setBackground(color2);
+    button3 = new JButton("Zapisz punkty");
+    button3.setBackground(color2);
+    grupa= new ButtonGroup();
+    grupa.add(radiobutton1);
+    grupa.add(radiobutton2);
+    radiobutton1.setSelected(true);
+    panel1.add(scorelabel);
+    panel1.add(sclabel);
+    panel1.add(radiobutton1);
+    panel1.add(radiobutton2);
+    panel1.add(button1);
+    panel1.add(button2);
+    panel1.add(button3);
+    this.add(panel1, BorderLayout.LINE_END);
+	}
+
+}
---
+++ BlocksBreaker/src/projekt/StartFrame.java
@@ -0,0 +1,80 @@
+package projekt;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.print.DocFlavor.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+  
+public class StartFrame extends JFrame {
+	JButton buttonlatwy, buttontrudny;
+	JLabel label1;
+	int fontSize=32;
+	Font font=new Font("Helvetica", Font.BOLD, fontSize);
+	JMenuBar menubar;
+	JMenu menu;
+	JMenuItem item1, item2;
+	StartFrame(){
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	    this.setSize(900,600);
+	    this.setTitle("BlocksBreaker");
+	    this.setResizable(false); 
+	    JLabel label = new JLabel();
+		ImageIcon image = new ImageIcon("image.png");
+		label.setIcon(image);
+	    label.setLayout(new GridLayout(4, 5,30, 20));
+	    label1=new JLabel("Wybierz poziom trudnoœci gry");
+	    label1.setFont(font);
+	    label1.setVerticalAlignment(SwingConstants.CENTER);
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        Color color1 = new Color(245, 244, 151);
+	    buttonlatwy = new JButton("£atwy");
+	    buttonlatwy.setBackground(color1);
+	    ActionListener latwy = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			  
+				Main frame = new Main();
+		        frame.setVisible(true);;
+			}	
+		};
+		buttonlatwy.addActionListener(latwy);
+	    buttontrudny = new JButton("Trudny");
+	    buttontrudny.setBackground(color1);
+	    label.add(label1);
+	    label.add(buttonlatwy);
+	    label.add(buttontrudny);
+		this.add(label);
+	    
+
+	    //MENUJÊZYK
+	    menubar = new JMenuBar();
+	    menu = new JMenu("Jêzyk");
+	    item1 = new JMenuItem("polski");
+	    item2 = new JMenuItem("angielski");
+	    menu.add(item1);
+	    menu.add(item2);
+	    menubar.add(menu);
+	    this.setJMenuBar(menubar);
+	}
+	public static void main(String[] args) {
+		
+		StartFrame okno= new StartFrame();
+		okno.setVisible(true);
+        
+	}
+
+}
