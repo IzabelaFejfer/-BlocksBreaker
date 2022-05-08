5388ba0b8a4e61fcc454cac767802d52885da456
IzabelaFejfer <izabela.fejfer@wp.pl>
2022-05-08 19:42:56 +0200

    Poprawki po merge

--- BlocksBreaker/src/projekt/StartFrame.java
+++ BlocksBreaker/src/projekt/StartFrame.java
@@ -14,17 +14,16 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
   
-public class StartFrame extends JFrame 
-{
-	JButton buttonEasy, buttonHard;
+public class StartFrame extends JFrame {
+	JButton buttonlatwy, buttontrudny;
 	JLabel label1;
 	int fontSize=33;
 	Font font=new Font("Helvetica", Font.BOLD, fontSize);
-	JMenuBar menuBar;
+	JMenuBar menubar;
 	JMenu menu;
 	JMenuItem item1, item2;
-	StartFrame()
-	{
+	
+	StartFrame(){
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	    this.setSize(900,600);
 	    this.setTitle("BlocksBreaker");
@@ -33,44 +32,54 @@ public class StartFrame extends JFrame
 		ImageIcon image = new ImageIcon("images/image.png");
 		label.setIcon(image);
 	    label.setLayout(new GridLayout(4, 5,30, 20));
-	    label1=new JLabel("Wybierz poziom trudnosci gry");
+	    label1=new JLabel("Wybierz poziom trudnoœci gry");
 	    label1.setFont(font);
 	    label1.setVerticalAlignment(SwingConstants.CENTER);
         label1.setHorizontalAlignment(SwingConstants.CENTER);
         Color color1 = new Color(245, 244, 151);
-	    buttonEasy = new JButton("Latwy");
-	    buttonEasy.setBackground(color1);
-	    ActionListener difficultyLevel = new ActionListener() 		//ActionListener otwierajacy nowe okno z gra
-	    {
+	    buttonlatwy = new JButton("£atwy");
+	    buttonlatwy.setBackground(color1);
+	    ActionListener latwy = new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) 
-			{
-				Main frame = new Main();
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Gameeasy frame = new Gameeasy();
+		        frame.setVisible(true);
+			}	
+		};
+		buttonlatwy.addActionListener(latwy);
+		
+	    buttontrudny = new JButton("Trudny");
+	    buttontrudny.setBackground(color1);
+	    ActionListener trudny = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Gamehard frame = new Gamehard();
 		        frame.setVisible(true);;
 			}	
 		};
-		buttonEasy.addActionListener(difficultyLevel);
-	    buttonHard = new JButton("Trudny");
-	    buttonHard.addActionListener(difficultyLevel);
-	    buttonHard.setBackground(color1);
+	    buttontrudny.addActionListener(trudny);
 	    label.add(label1);
-	    label.add(buttonEasy);
-	    label.add(buttonHard);
+	    label.add(buttonlatwy);
+	    label.add(buttontrudny);
 		this.add(label);
 	    
-	    //MENU
-	    menuBar = new JMenuBar();
-	    menu = new JMenu("Jezyk");
+	    //MENUJÊZYK
+	    menubar = new JMenuBar();
+	    menu = new JMenu("Jêzyk");
 	    item1 = new JMenuItem("polski");
 	    item2 = new JMenuItem("angielski");
 	    menu.add(item1);
 	    menu.add(item2);
-	    menuBar.add(menu);
-	    this.setJMenuBar(menuBar);
+	    menubar.add(menu);
+	    this.setJMenuBar(menubar);
 	}
-	public static void main(String[] args) 
-	{
-		StartFrame frame = new StartFrame();
-		frame.setVisible(true);
+	public static void main(String[] args) {
+		
+		StartFrame okno= new StartFrame();
+		okno.setVisible(true);
+        
 	}
+
 }
