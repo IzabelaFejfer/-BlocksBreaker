11a72de1cf53ab35ab6465925539b64a3d199491
IzabelaFejfer <izabela.fejfer@wp.pl>
2022-06-13 18:28:18 +0200

    Wersja 1

--- BlocksBreaker/src/projekt/StartFrame.java
+++ BlocksBreaker/src/projekt/StartFrame.java
@@ -27,71 +27,16 @@ public class StartFrame extends JFrame {
 	JMenuBar menubar;
 	JMenu menu;
 	JMenuItem item1, item2;
+	Color zolty = new Color(245, 244, 151);
 
-	
-//	StartFrame(){
-//		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//	    this.setSize(900,600);
-//	    this.setTitle("BlocksBreaker");
-//	    this.setResizable(false); 
-//	    JLabel label = new JLabel();
-//		ImageIcon image = new ImageIcon("images/image.png");
-//		label.setIcon(image);
-//	    label.setLayout(new GridLayout(4, 5,30, 20));
-//	    label1=new JLabel("Wybierz poziom trudnoœci gry");
-//	    label1.setFont(font);
-//	    label1.setVerticalAlignment(SwingConstants.CENTER);
-//        label1.setHorizontalAlignment(SwingConstants.CENTER);
-//        Color color1 = new Color(245, 244, 151);
-//	    buttonlatwy = new JButton("£atwy");
-//	    buttonlatwy.setBackground(color1);
-//	    ActionListener latwy = new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent arg0) {
-//				
-//				Gameeasy frame = new Gameeasy();
-//		        frame.setVisible(true);
-//			}	
-//		};
-//		buttonlatwy.addActionListener(latwy);
-//		
-//	    buttontrudny = new JButton("Trudny");
-//	    buttontrudny.setBackground(color1);
-//	    ActionListener trudny = new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent arg0) {
-//				
-//				Gamehard frame = new Gamehard();
-//		        frame.setVisible(true);;
-//			}	
-//		};
-//	    buttontrudny.addActionListener(trudny);
-//	    label.add(label1);
-//	    label.add(buttonlatwy);
-//	    label.add(buttontrudny);
-//		this.add(label);
-//	    
-//	    //MENUJÊZYK
-//	    menubar = new JMenuBar();
-//	    menu = new JMenu("Jêzyk");
-//	    item1 = new JMenuItem("polski");
-//	    item2 = new JMenuItem("angielski");
-//	    menu.add(item1);
-//	    menu.add(item2);
-//	    menubar.add(menu);
-//	    this.setJMenuBar(menubar);
-//	}
-	
-	
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable(){
+		SwingUtilities.invokeLater(new Runnable() {
 			 public void run() {
-		StartFrame okno= new StartFrame();
-		okno.setVisible(true);
+				StartFrame okno= new StartFrame();
+				okno.setVisible(true);
 			 }
-	});
-		
-}
+		});	
+	}
 	
 	public StartFrame() throws HeadlessException {
 		super();
@@ -102,36 +47,38 @@ public class StartFrame extends JFrame {
 	    JLabel label = new JLabel();
 		ImageIcon image = new ImageIcon("images/image.png");
 		label.setIcon(image);
-	    label.setLayout(new GridLayout(4, 5,30, 20));
+	    label.setLayout(new GridLayout(4, 5, 30, 20));
 	    label1=new JLabel("Wybierz poziom trudnoœci gry");
 	    label1.setFont(font);
 	    label1.setVerticalAlignment(SwingConstants.CENTER);
         label1.setHorizontalAlignment(SwingConstants.CENTER);
-        Color color1 = new Color(245, 244, 151);
 	    buttonlatwy = new JButton("£atwy");
-	    buttonlatwy.setBackground(color1);
+	    buttonlatwy.setBackground(zolty);
 	    
 	    ActionListener latwy = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				 synchronized (this){
-				Gameeasy frame = new Gameeasy();
-		        frame.setVisible(true);
+						Gameeasy frame = new Gameeasy();
+				        frame.setVisible(true);
 				 }
 			}	
 		};
-		buttonlatwy.addActionListener(latwy);
 		
+		buttonlatwy.addActionListener(latwy);
 	    buttontrudny = new JButton("Trudny");
-	    buttontrudny.setBackground(color1);
+	    buttontrudny.setBackground(zolty);
+	    
 	    ActionListener trudny = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				Gamehard frame = new Gamehard();
-		        frame.setVisible(true);;
-			}	
+				synchronized (this){
+						Gamehard frame = new Gamehard();
+				        frame.setVisible(true);;
+				}	
+			}
 		};
+		
 	    buttontrudny.addActionListener(trudny);
 	    label.add(label1);
 	    label.add(buttonlatwy);
@@ -147,7 +94,5 @@ public class StartFrame extends JFrame {
 	    menu.add(item2);
 	    menubar.add(menu);
 	    this.setJMenuBar(menubar);
-	    
 	}
-	
 }
